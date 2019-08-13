5dca68e009a44ccb6dbcb8c19f0fac931f3b9da2
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-08-13 00:02:22 -0400

    Add 2 digits after decimal point

--- bamazon.sql
+++ bamazon.sql
@@ -12,24 +12,16 @@ CREATE TABLE products (
 );
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUE 
-("MacBook Pro", "Electronics", 1699, 10),
+("MacBook Pro", "Electronics", 1699.00, 10),
 ("iRobot Roomba", "Electronics", 699.99, 10),
-("Vitamix", "Kitchen", 550, 10),
+("Vitamix", "Kitchen", 550.00, 10),
 ("Coffee Grinder", "Kitchen", 150, 10),
-("Rocking Chair", "Home", 100, 20),
-("Aroma Difuser", "Home", 35, 20),
-("Throw Blanket", "Home", 25, 20),
+("Rocking Chair", "Home", 100.00, 20),
+("Aroma Difuser", "Home", 35.00, 20),
+("Throw Blanket", "Home", 25.00, 20),
 ("Almond Milk", "Food", 4.99, 50),
 ("Protain Bar", "Food", 2.99, 50),
 ("Gummy Candy", "Food", 1.99, 50);
-
-CREATE TABLE summary (
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(30),
-  price DECIMAL(10,2),
-  quantity INT(10),
-  PRIMARY KEY(item_id)
-);
 
 SELECT * FROM products;
 SELECT * FROM summary;
